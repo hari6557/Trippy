61dc5dc7e37e6d7f45b2cc6ced0313a3a94de1df
Harikrishnan s <harikrishnansajikumar123@gmail.com>
2021-06-20 22:16:44 +0530

    Updates

---
+++ precache-manifest.d287d6dc4744c2b0c3733e1bd3652ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea68ab8c4f607753715d658d18dbc7e",
+    "url": "/Trippy/index.html"
+  },
+  {
+    "revision": "47897a5d68662abb7c81",
+    "url": "/Trippy/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/Trippy/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Trippy/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47897a5d68662abb7c81",
+    "url": "/Trippy/static/js/main.57ebee44.chunk.js"
+  },
+  {
+    "revision": "e03a2c08c21964ae16f2",
+    "url": "/Trippy/static/js/runtime-main.177ccef0.js"
+  }
+]);
